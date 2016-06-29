2ffe42e6d50bd289a40240db30ee30dab049f2a2
Tomas Mikula <tomas.mikula@gmail.com>
2016-06-29 14:04:04 -0400

    Add methods ProteinModifications.addModification and .meet

--- src/main/scala/protein/mechanism/ProteinModifications.scala
+++ src/main/scala/protein/mechanism/ProteinModifications.scala
@@ -6,6 +6,13 @@ case class ProteinModifications(mods: Map[Site, SiteState]) extends AnyVal {
 
   def get(s: Site): Option[SiteState] = mods.get(s)
 
+  def addModification(site: Site, state: SiteState): Option[ProteinModifications] = {
+    mods.get(site).fold(
+      Option(ProteinModifications(mods.updated(site, state))))(
+      st => if(st == state) Some(this) else None
+    )
+  }
+
   def combine(that: ProteinModifications): Option[ProteinModifications] = {
 
     val entries: Iterator[Option[(Site, SiteState)]] =
@@ -29,6 +36,20 @@ case class ProteinModifications(mods: Map[Site, SiteState]) extends AnyVal {
 
     if (!isCollision) Some(ProteinModifications(builder.toMap))
     else None
+  }
+
+  /** Returns ProteinModifications that is less specific than either `this` or `that`
+    * and is the most specific such ProteinModifications. In other words, returns the
+    * greatest lower bound of the two in the (partial) ordering given by specificity.
+    */
+  def meet(that: ProteinModifications): ProteinModifications = {
+    val keys = this.mods.keySet union that.mods.keySet
+    val builder = mutable.Map[Site, SiteState]()
+    keys.foreach(s => (this.get(s), that.get(s)) match {
+      case (Some(s1), Some(s2)) if s1 == s2 => builder.put(s, s1)
+      case _ => // do nothing
+    })
+    ProteinModifications(builder.toMap)
   }
 
   override def toString = mods.iterator.map({ case (s, st) => s"$s~$st" }).mkString("(", ",", ")")
