4346558c873e67e712541558e0dae2b6661963d3
Tomas Mikula <tomas.mikula@gmail.com>
2016-08-29 17:44:06 -0400

    Move ProteinModificationsLattice to ProteinModifications companion.

--- src/main/scala/proteinrefinery/lib/ProteinModifications.scala
+++ src/main/scala/proteinrefinery/lib/ProteinModifications.scala
@@ -1,5 +1,7 @@
 package proteinrefinery.lib
 
+import nutcracker.{Dom, Join}
+
 import scala.collection.mutable
 
 case class ProteinModifications(mods: Map[Site, SiteState]) extends AnyVal {
@@ -57,4 +59,26 @@ case class ProteinModifications(mods: Map[Site, SiteState]) extends AnyVal {
 
 object ProteinModifications {
   def noModifications: ProteinModifications = ProteinModifications(Map())
+
+  type ProteinModificationsLattice = Option[ProteinModifications]
+
+  implicit def proteinModificationsLattice: Dom.Aux[ProteinModificationsLattice, Join[ProteinModificationsLattice], Unit] =
+    new Dom[ProteinModificationsLattice] {
+      type Update = Join[ProteinModificationsLattice]
+      type Delta = Unit
+      override def assess(d: ProteinModificationsLattice): Dom.Status[Join[ProteinModificationsLattice]] = d match {
+        case None => Dom.Failed
+        case Some(a) => Dom.Refined
+      }
+
+      override def update(d: ProteinModificationsLattice, u: Join[ProteinModificationsLattice]): Option[(ProteinModificationsLattice, Unit)] = {
+        val res = (d, u.value) match {
+          case (Some(p), Some(q)) => p combine q
+          case _ => None
+        }
+        if(res == d) None else Some((res, ()))
+      }
+
+      override def combineDeltas(d1: Unit, d2: Unit): Unit = ()
+    }
 }
